ad64ef5c3b17b860e3a2be4713b9ce7162704ecb
nancy-chaalan <150782100+nancy-chaalan@users.noreply.github.com>
2023-12-15 10:16:19 +0200

    add

--- src/app/services/video-service.service.ts
+++ src/app/services/video-service.service.ts
@@ -12,11 +12,13 @@ export class VideoServiceService{
   }
 
   allVideos: any [] = [
-    {id: 1, videoID:"2X5g0nUojA4", img:"./assets/videos/img1.png",title:"C3D Site with Spline and React - Full Course", channelName:"Channel", date:"12/1/2023", views:12,smallImg: 'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 43, likes: 12},
-    {id: 2, videoID:"5bTw_Q9po_s", img:"./assets/videos/img2.png",title:"bla bla bla", channelName:"youtube", date:"1/7/2023", views:17,smallImg: 'assets/videos/profilePic/b.png',  videoTime: '12:30', subscribers: 43, likes: 1200},
+    {id: 1, videoID:"2X5g0nUojA4", img:"./assets/videos/img2.png",title:"C3D Site with Spline and React - Full Course", channelName:"Channel", date:"12/1/2023", views:12,smallImg: 'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 43, likes: 12},
+    {id: 2, videoID:"5bTw_Q9po_s", img:"./assets/videos/img1.png",title:"bla bla bla", channelName:"youtube", date:"1/7/2023", views:17,smallImg: 'assets/videos/profilePic/b.png',  videoTime: '12:30', subscribers: 43, likes: 1200},
     {id: 3, videoID:"KgA2TpKJC30", img:"assets/videos/img3.png",title:"Create 3D Site with Spline and React - Full Course",  channelName:"word", date:"5/1/2023",  views:13,smallImg:'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 63, likes: 3002},
     {id: 4, videoID:"5bTw_Q9po_s", img:"./assets/videos/img13.png",title:"bla bla bla", channelName:"youtube", date:"1/7/2023", views:17,smallImg: 'assets/videos/profilePic/b.png',  videoTime: '12:30', subscribers: 43, likes: 1200},
     {id: 5, videoID:"KgA2TpKJC30", img:"assets/videos/img11.jpeg",title:"Create 3D Site with Spline and React - Full Course",  channelName:"word", date:"5/1/2023",  views:13,smallImg:'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 63, likes: 3002},
+    {id: 6, videoID:"2X5g0nUojA4", img:"./assets/videos/img8.png",title:"Spline and React - Full Course", channelName:"Channel", date:"12/1/2023", views:12,smallImg: 'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 43, likes: 12},
+    {id: 7, videoID:"2X5g0nUojA4", img:"./assets/videos/img9.png",title:"Site with Spline and React - Full Course", channelName:"Channel", date:"12/1/2023", views:12,smallImg: 'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 43, likes: 12},
     {id: 8, videoID:"2X5g0nUojA4", img:"./assets/videos/img4.png",title:"Full Course-C3D Site with Spline and React", channelName:"Channel", date:"12/1/2023", views:12,smallImg: 'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 43, likes: 12},
     {id: 9, videoID:"2X5g0nUojA4", img:"./assets/videos/img5.png",title:"React - Full Course", channelName:"Channel", date:"12/1/2023", views:12,smallImg: 'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 43, likes: 12},
     {id: 10, videoID:"2X5g0nUojA4", img:"./assets/videos/img6.png",title:"Cand React - Full Course", channelName:"Channel", date:"12/1/2023", views:12,smallImg: 'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 43, likes: 12},
@@ -24,6 +26,7 @@ export class VideoServiceService{
     {id: 12, videoID:"2X5g0nUojA4", img:"./assets/videos/img10.png",title:"Site with Spline and React - Full Course", channelName:"Channel", date:"12/1/2023", views:12,smallImg: 'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 43, likes: 12},
     {id: 13, videoID:"2X5g0nUojA4", img:"./assets/videos/img12.png",title:"with Spline and React - Full Course", channelName:"Channel", date:"12/1/2023", views:12,smallImg: 'assets/videos/profilePic/a.png', videoTime: '4:30', subscribers: 43, likes: 12},
   ]
+  
 
 arrayNeeded: any [] = [];
 
